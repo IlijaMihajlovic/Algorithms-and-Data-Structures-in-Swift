e0f79353084246d131d7f91d76c7c4f690fc9e99
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-10-21 16:11:14 +0200

    minor change

--- Array Coding Challenges/Check word or letter count in a sentence.playground/Contents.swift
+++ Array Coding Challenges/Check word or letter count in a sentence.playground/Contents.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 //1.
-func SpecificLetterCount(str:String, char:Character) ->Int {
+func specificLetterCount(str:String, char:Character) ->Int {
     let letters = str
     var count = 0
     
@@ -15,7 +15,7 @@ func SpecificLetterCount(str:String, char:Character) ->Int {
 }
 
 //2.
-func SpecificWordCount(forString:String, inWord:String) ->Int {
+func specificWordCount(forString:String, inWord:String) ->Int {
     let words = forString.components(separatedBy: " ")
     var count = 0
     
@@ -29,17 +29,17 @@ func SpecificWordCount(forString:String, inWord:String) ->Int {
 }
 
 //3.
-func CountAllWords(str:String) ->[String: Int] {
+func countAllWords(str:String) ->[String: Int] {
 
     let words = str.components(separatedBy: " ")
     var wordcount = [String: Int]()
     words.forEach { (word) in
-        wordcount.updateValue(SpecificWordCount(forString: str, inWord: word), forKey: word)
+        wordcount.updateValue(specificWordCount(forString: str, inWord: word), forKey: word)
     }
 
     return wordcount
 }
 
 
-let wordCount = SpecificWordCount(forString: "Il est mon meilleur ami il est mon meilleur ami tho", inWord: "est")
+let wordCount = specificWordCount(forString: "Il est mon meilleur ami il est mon meilleur ami tho", inWord: "est")
 print("Word Count: " + String(describing: wordCount))
