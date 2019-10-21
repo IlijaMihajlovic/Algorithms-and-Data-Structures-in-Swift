d3a63e0e830ed60613757f23002cda06d98e09dc
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-10-21 13:06:58 +0200

    Add check word or letter count in a sentence functions

---
+++ Array Challenges/Check word or letter count in a sentence.playground/Contents.swift
@@ -0,0 +1,46 @@
+
+import UIKit
+
+//1.
+func SpecificLetterCount(str:String, char:Character) ->Int {
+    let letters = Array(str)
+    var count = 0
+    
+    letters.forEach { (letter) in
+        if letter == char {
+            count += 1
+        }
+    }
+    return count
+}
+
+
+//2.
+func SpecificWordCount(forString:String, inWord:String) ->Int {
+    let words = forString.components(separatedBy: " ")
+    var count = 0
+    
+    words.forEach { (word) in
+        if word == inWord {
+            count += 1
+        }
+    }
+    
+    return count
+}
+
+//3.
+func CountAllWords(str:String) ->[String: Int] {
+
+    let words = str.components(separatedBy: " ")
+    var wordcount = [String: Int]()
+    words.forEach { (word) in
+        wordcount.updateValue(SpecificWordCount(forString: str, inWord: word), forKey: word)
+    }
+
+    return wordcount
+}
+
+
+let wordCount = SpecificWordCount(forString: "Il est mon meilleur ami il est mon meilleur ami tho", inWord: "est")
+print("Word Count: " + String(describing: wordCount))
