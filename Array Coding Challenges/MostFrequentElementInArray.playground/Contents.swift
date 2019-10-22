6650b91d252e965bba37226132ebe191d04c80f7
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-10-22 11:24:31 +0200

    modify the function that returns the most frequent element

--- Array Coding Challenges/MostFrequentElementInArray.playground/Contents.swift
+++ Array Coding Challenges/MostFrequentElementInArray.playground/Contents.swift
@@ -7,13 +7,10 @@ func mostFrequent<T: Hashable>(array: [T]) -> (value: T, count: Int)? {
     //The dictionary that holds the count for each element in the array
     let counts = array.reduce(into: [:]) { $0[$1, default: 0] += 1 }
 
-    // Find the most frequent value and its count with max(by:)
-    if let (value, count) = counts.max(by: { $0.1 < $1.1 }) {
-        return (value, count)
-    }
-
-    // array was empty
-    return nil
+    //Find the most frequent value and its count with max(by:) else array was empty
+   guard let (value, count) = counts.max(by: { $0.1 < $1.1}) else { return nil }
+   
+   return (value, count)
 }
 
 if let result = mostFrequent(array: [1,2,4,5,6,7,8,8,8]) {
